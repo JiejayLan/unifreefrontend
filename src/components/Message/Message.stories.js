572f7c15d57453228a5218230635e573eca4e2e5
EngerThen <engerthen4d17@gmail.com>
2019-10-14 20:05:15 -0400

    added storybook.js

---
+++ src/components/Message/Message.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Message } from './Message';
+
+
+// export title of the component
+export default { title: 'Message' };
+
+// write a story for this component
+export const withText = () => <Message />;
