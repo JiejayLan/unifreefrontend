e66db2039b586c5d7464c06dd0f0c227734b8fbd
EngerThen <engerthen4d17@gmail.com>
2019-10-21 23:09:57 -0400

    example components

--- src/components/Message/Message.stories.js
+++ src/components/Message/Message.stories.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { action } from '@storybook/addon-actions';
 import { Message } from './Message';
-
 
 // export title of the component
 export default { title: 'Message' };
 
 // write a story for this component
 export const withText = () => <Message />;
+
+
+export const withButton = () => <button type="submit" onSubmit={action('clicked')}>Example</button>;
---
+++ src/components/Task/Task.js
@@ -0,0 +1,22 @@
+// src/components/Task.js
+
+import React from 'react';
+import { string, shape } from 'prop-types';
+import './Task.css';
+
+export function Task({ task: { title, state, checked } }) {
+  return (
+    <div className={`list-item ${state}`}>
+      <input type="checkbox" checked={checked} />
+      <span>{ title }</span>
+    </div>
+  );
+}
+
+
+Task.propTypes = {
+  task: shape({
+    title: string.isRequired,
+    state: string.isRequired,
+  }).isRequired,
+};
