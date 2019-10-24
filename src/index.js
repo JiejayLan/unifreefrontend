f6cbb734acfc1fa73e76e58be1582002687db028
Nan Chen <Nan.Chen@nyct.com>
2019-10-24 11:16:25 -0400

    added theme colors and font sizes

--- src/index.js
+++ src/index.js
@@ -1,22 +1,31 @@
 import './index.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
 import { AppRouter } from './routes/AppRouter';
 
 const theme = createMuiTheme({
-  typography: {
-    fontFamily: [
-      'Roboto',
-      'Arial',
-      'sans-serif',
-    ].join(','),
-  },
+  palette: {
+    primary: {
+      main: '#7d5cc6'
+    },
+    secondary: {
+      main: '#333333'
+    },
+    surface: {
+      main: '#d2d2d2'
+    },
+    text: {
+      primary: '#ffffff',
+      secondary: '#464646',
+      contrast: '#f3cf45'
+    }
+  }
 });
 
 ReactDOM.render(
-  <ThemeProvider theme={theme}>
+  <MuiThemeProvider theme={theme}>
     <AppRouter />
-  </ThemeProvider>,
-  document.getElementById('root'),
+  </MuiThemeProvider>,
+  document.getElementById('root')
 );
