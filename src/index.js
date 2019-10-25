de271e48cb6a9e876760766136a73bdf10af93db
Nan Chen <nanchen432@gmail.com>
2019-10-24 21:02:13 -0400

    change MuiThemeProvider to ThemeProvider

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
 import './index.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import { AppRouter } from './routes/AppRouter';
 
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: '#7d5cc6'
+      main: '#7d5cc6',
     },
     secondary: {
-      main: '#333333'
+      main: '#333333',
     },
     surface: {
-      main: '#d2d2d2'
+      main: '#d2d2d2',
     },
     text: {
       primary: '#ffffff',
       secondary: '#464646',
-      contrast: '#f3cf45'
-    }
-  }
+      contrast: '#f3cf45',
+    },
+  },
 });
 
 ReactDOM.render(
-  <MuiThemeProvider theme={theme}>
+  <ThemeProvider theme={theme}>
     <AppRouter />
-  </MuiThemeProvider>,
-  document.getElementById('root')
+  </ThemeProvider>,
+  document.getElementById('root'),
 );
