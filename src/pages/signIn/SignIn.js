f3eff8b85120e52f9e71ef0abe261392858a89cf
Jie Lan <lanjie3458@gmail.com>
2019-10-25 23:22:08 -0400

    change SignIn page style

--- src/pages/signIn/SignIn.js
+++ src/pages/signIn/SignIn.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { Grid } from '@material-ui/core';
 import { SignInForm } from '../../components/Form/SignInForm';
 
 export const SignIn = () => (
-  <SignInForm />
+  <Grid container justify="center">
+    <SignInForm />
+  </Grid>
 );
