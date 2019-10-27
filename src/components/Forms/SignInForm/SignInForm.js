686300b0dffef47ad1af00cb84e63f376119878f
Jie Lan <lanjie3458@gmail.com>
2019-10-26 20:33:02 -0400

    change api domain config

--- src/components/Forms/SignInForm/SignInForm.js
+++ src/components/Forms/SignInForm/SignInForm.js
@@ -8,7 +8,7 @@ import { ErrorMessage } from '../../ErrorMessage';
 
 const errorMsg = 'Invalid username or password';
 const path = '/api/v1/signin';
-const domain = config.api_domain.replace('\'', '');
+const domain = config.apiDomain;
 
 export const SignInForm = () => {
   const [formData, setForm] = useState({ username: '', password: '' });
