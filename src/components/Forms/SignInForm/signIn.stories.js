f1d672d56a149147f244744c794e815031554159
Jie Lan <lanjie3458@gmail.com>
2019-10-31 16:47:23 -0400

    add signin Page story

---
+++ src/components/Forms/SignInForm/signIn.stories.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+import { SignInForm } from './SignInForm';
+
+storiesOf('Form', module)
+  .add('SignIn Form',
+    withInfo({
+      text: 'description: User login with username and password',
+      inline: false,
+      source: false,
+    })(() => <SignInForm />),
+    {
+      notes: 'note',
+    });
