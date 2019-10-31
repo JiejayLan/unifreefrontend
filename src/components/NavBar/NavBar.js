1e04053b49f1ecb12679615a27b22156bbcdf25a
Nan Chen <nanchen432@gmail.com>
2019-10-30 22:13:49 -0400

    clear jwtToken when signout

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,30 +7,38 @@ import {
 import ForumRoundedIcon from '@material-ui/icons/ForumRounded';
 import { bool } from 'prop-types';
 
-export const NavBar = ({ isAuthenticated }) => (
-  <AppBar position="static" className="nav-bar">
-    <Toolbar>
-      <ForumRoundedIcon edge="start" aria-label="UniFree-forum" />
-      <Typography variant="h6" className="title">
-        UniFree
-      </Typography>
-      {isAuthenticated ? (
-        <Button color="inherit" component={Link} to="/">
-          Logout
-        </Button>
-      ) : (
-        <>
-          <Button color="inherit" component={Link} to="/">
-              Login
+export const NavBar = ({ isAuthenticated }) => {
+  function handleSignOut() {
+    if (window.localStorage.jwtToken) {
+      window.localStorage.clear();
+    }
+  }
+
+  return (
+    <AppBar position="static" className="nav-bar">
+      <Toolbar>
+        <ForumRoundedIcon edge="start" aria-label="UniFree-forum" />
+        <Typography variant="h6" className="title">
+          UniFree
+        </Typography>
+        {isAuthenticated ? (
+          <Button color="inherit" component={Link} to="/" onClick={handleSignOut}>
+            Logout
           </Button>
-          <Button variant="contained" component={Link} to="/">
-              Signup
-          </Button>
-        </>
-      )}
-    </Toolbar>
-  </AppBar>
-);
+        ) : (
+            <>
+              <Button color="inherit" component={Link} to="/">
+                Login
+              </Button>
+              <Button variant="contained" component={Link} to="/">
+                Signup
+              </Button>
+            </>
+          )}
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 NavBar.defaultProps = {
   isAuthenticated: false,
