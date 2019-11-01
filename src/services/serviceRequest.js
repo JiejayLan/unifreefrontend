e10b01e5dee3d81e59e42d3446d27ab21f3de019
Jie Lan <lanjie3458@gmail.com>
2019-10-31 20:26:31 -0400

    update serviceRequest

--- src/services/serviceRequest.js
+++ src/services/serviceRequest.js
@@ -3,7 +3,7 @@ import request from 'axios';
 export async function serviceRequest(requestInfo) {
   try {
     // eslint-disable-next-line no-unused-vars
-    const response = await request({ ...requestInfo });
+    const response = await request({ ...requestInfo, validateStatus: (_) => true });
     const { data } = response;
     return data;
   } catch (err) {
