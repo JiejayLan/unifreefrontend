f0baa8448a960d7898ea6b77b67588db9dac14c6
EngerThen <engerthen4d17@gmail.com>
2019-11-02 06:50:42 -0700

    Testing form SignUpForm

---
+++ src/components/Forms/SignUpForm/__tests__/SignUpForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { SignUpForm } from '../SignUpForm';
+
+describe('SignUpForm Test Suite', () => {
+  it('Should render without crashing', () => {
+    const { getByText } = render(<SignUpForm />);
+    expect(getByText('signUp')).toBeInTheDocument();
+  });
+});
