fa685b7ea2a6455e114e2afd4dd428b8fa4f6875
Nan Chen <nanchen432@gmail.com>
2019-11-01 22:05:27 -0400

    remove scss; change theme color palette

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,13 +1,16 @@
-import './NavBar.scss';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import {
   AppBar, Toolbar, Button, Typography,
 } from '@material-ui/core';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import ForumRoundedIcon from '@material-ui/icons/ForumRounded';
 import { bool } from 'prop-types';
+import useStyles from './style';
+
 
 export const NavBar = ({ isAuthenticated }) => {
+  const classes = useStyles();
   const handleSignOut = () => {
     if (window.localStorage.jwtToken) {
       window.localStorage.clear();
@@ -15,28 +18,37 @@ export const NavBar = ({ isAuthenticated }) => {
   };
 
   return (
-    <AppBar position="static" className="nav-bar">
-      <Toolbar>
-        <ForumRoundedIcon edge="start" aria-label="UniFree-forum" />
-        <Typography variant="h6" className="title">
-          UniFree
-        </Typography>
-        {isAuthenticated ? (
-          <Button data-testid="logout-button" color="inherit" component={Link} to="/" onClick={handleSignOut}>
-            Logout
-          </Button>
-        ) : (
-          <>
-            <Button data-testid="login-button" color="inherit" component={Link} to="/signin">
-                Login
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="static">
+        <Toolbar>
+          <ForumRoundedIcon edge="start" aria-label="UniFree-forum" />
+          <Typography variant="h6" className={classes.title}>
+            UniFree
+          </Typography>
+          {isAuthenticated ? (
+            <Button data-testid="logout-button" color="inherit" component={Link} to="/" onClick={handleSignOut}>
+              Logout
             </Button>
-            <Button data-testid="signup-button" variant="contained" component={Link} to="/">
-                Signup
-            </Button>
-          </>
-        )}
-      </Toolbar>
-    </AppBar>
+          ) : (
+              <>
+                <Button
+                  className={classes.button}
+                  data-testid="login-button"
+                  color="inherit"
+                  component={Link}
+                  to="/signin"
+                >
+                  Login
+              </Button>
+                <Button data-testid="signup-button" variant="contained" component={Link} to="/">
+                  Signup
+              </Button>
+              </>
+            )}
+        </Toolbar>
+      </AppBar>
+    </div>
   );
 };
 
