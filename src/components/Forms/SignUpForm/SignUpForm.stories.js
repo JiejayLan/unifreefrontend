ec2ead15203cb88698f33acfd22401f844ec8e7f
EngerThen <engerthen4d17@gmail.com>
2019-11-02 06:50:16 -0700

    story documentation for SignUpForm

---
+++ src/components/Forms/SignUpForm/SignUpForm.stories.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { SignUpForm } from './SignUpForm';
+
+storiesOf('SignUpForm', module).add('default', () => (
+  <SignUpForm />
+));
