6894861ba5c7851f744e52a68d0722d682314018
Yu Ting Yang <yuting7376@gmail.com>
2019-11-10 04:41:46 -0500

    add unit test to individual post component

---
+++ src/components/Posts/IndividualPost/__tests__/IndividualPost.js
@@ -0,0 +1,49 @@
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import React from 'react';
+import { IndividualPost } from '../IndividualPost';
+import { serviceRequest } from '../../../../services/serviceRequest';
+
+jest.mock('../../../../services/serviceRequest');
+
+describe('Individual Post test', () => {
+  const successPayLoad = {
+    status: 'success',
+    data: {
+      label: 'sample',
+      title: 'sample title',
+      content: 'This is a sample content for the sample post with sample title',
+      updatedAt: '2019-11-07T09:26:51.822Z',
+      username: 'sample',
+      postID: 1,
+    },
+  };
+
+  const failPayLoad = {
+    status: 'error',
+    message: 'Cannot read property "dataValues" of null',
+  };
+
+  beforeAll(() => {
+    // eslint-disable-next-line no-console
+    console.error = jest.fn();
+  });
+
+  beforeEach(() => {
+    serviceRequest.mockClear();
+  });
+
+  it('should render without crash', () => {
+    serviceRequest.mockImplementation(async () => (successPayLoad));
+    const { baseElement } = render(<IndividualPost />);
+
+    expect(baseElement.outerHTML).toBeDefined();
+  });
+
+  it('should catch error for internal service error', async () => {
+    serviceRequest.mockReturnValue(failPayLoad);
+    const { baseElement } = render(<IndividualPost />);
+    await new Promise((_) => setTimeout(_, 100));
+    expect(expect(baseElement.outerHTML).toBe('<body><div></div></body>'));
+  });
+});
