84c9abdff018afc2b8fa3770f2e86b3a52b18441
EngerThen <engerthen4d17@gmail.com>
2019-11-13 08:18:07 -0500

    added story

--- src/components/ElevationScroll/ElevationScroll.stories.js
+++ src/components/ElevationScroll/ElevationScroll.stories.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+import { ElevationScroll } from './ElevationScroll';
+
+export default {
+  component: ElevationScroll,
+  title: 'ElevationScroll',
+};
+
+storiesOf('ScrollEvents', module)
+  .add('Default', withInfo({
+    text: 'description: Wrapper component to make navbar not scroll',
+    inline: true,
+    source: false,
+  })(() => <ElevationScroll><p>Test</p></ElevationScroll>));
