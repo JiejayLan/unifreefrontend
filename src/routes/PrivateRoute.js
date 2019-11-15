c966184bd0eb02a5828c36565a639af770f7f1c7
EngerThen <engerthen4d17@gmail.com>
2019-11-15 03:41:22 -0500

    added ScrollToTop to website

--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -2,7 +2,10 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { func } from 'prop-types';
 import cookie from 'react-cookies';
+import { Fab } from '@material-ui/core';
+import { KeyboardArrowUp } from '@material-ui/icons';
 import { NavBar } from '../components/NavBar';
+import { ScrollToTop } from '../components/ScrollToTop';
 
 export const PrivateRoute = ({
   component: Component,
@@ -14,6 +17,11 @@ export const PrivateRoute = ({
       <>
         <NavBar isAuthenticated />
         <Component {...props} />
+        <ScrollToTop scrollStep={100} delayInMS={16.6}>
+          <Fab color="inherit" size="small" aria-label="scroll back to top">
+            <KeyboardArrowUp />
+          </Fab>
+        </ScrollToTop>
       </>
     ) : (
       <Redirect to="/signin" />
