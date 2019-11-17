80edc109d564f1a7457ad2dc0fa0631bf991c492
EngerThen <engerthen4d17@gmail.com>
2019-11-17 17:27:53 -0500

    fixed margin issue with the navbar

--- src/routes/PublicRoute.js
+++ src/routes/PublicRoute.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import { func } from 'prop-types';
 import cookie from 'react-cookies';
-import { Fab } from '@material-ui/core';
+import { Fab, Toolbar } from '@material-ui/core';
 import { KeyboardArrowUp } from '@material-ui/icons';
 import { NavBar } from '../components/NavBar';
 import { ScrollToTop } from '../components/ScrollToTop';
@@ -16,6 +16,7 @@ export const PublicRoute = ({
     component={(props) => (
       <>
         <NavBar isAuthenticated={!!cookie.load('jwtToken')} />
+        <Toolbar />
         <Component {...props} />
         <ScrollToTop scrollStep={100} delayInMS={16.6}>
           <Fab color="inherit" size="small" aria-label="scroll back to top">
