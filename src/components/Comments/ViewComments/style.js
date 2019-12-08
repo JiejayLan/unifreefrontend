e9912366a2a955f5e6243566aab072552b750136
EngerThen <engerthen4d17@gmail.com>
2019-12-07 22:29:12 -0500

    fixed bug where profile did not align with text

--- src/components/Comments/ViewComments/style.js
+++ src/components/Comments/ViewComments/style.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
   list: {
     marginBottom: theme.spacing(2),
+    alignItems: 'flex-start',
   },
   commentHeader: {
     marginTop: theme.spacing(5),
