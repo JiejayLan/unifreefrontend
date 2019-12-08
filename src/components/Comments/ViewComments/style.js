f398e2d81fdf47c8f657684506f8af472b37da1e
EngerThen <engerthen4d17@gmail.com>
2019-12-07 22:42:57 -0500

    Revert "fixed bug where profile did not align with text"
    This reverts commit e9912366a2a955f5e6243566aab072552b750136.

--- src/components/Comments/ViewComments/style.js
+++ src/components/Comments/ViewComments/style.js
@@ -3,7 +3,6 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
   list: {
     marginBottom: theme.spacing(2),
-    alignItems: 'flex-start',
   },
   commentHeader: {
     marginTop: theme.spacing(5),
